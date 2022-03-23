c63398f755f48dd92f2bd648c17c1906cef941aa
poonambudde <buddepoonam123@gmail.com>
2022-03-23 20:35:23 +0530

    [Poonam] Added: Creat a Employee PayRoll Table

--- PayRoll.sql
+++ PayRoll.sql
@@ -1,2 +1,13 @@
+--UC1 - Create Payroll_Service Database
 create database PayRoll_Service;
 use PayRoll_Service;
+
+--UC2 - Create employee_payroll table
+create table employee_payroll
+(
+  id int primary key not null identity(1,1),
+  nameofStudent varchar (150) not null,
+  salary float not null,
+  startDate date
+)
+select * from employee_payroll
